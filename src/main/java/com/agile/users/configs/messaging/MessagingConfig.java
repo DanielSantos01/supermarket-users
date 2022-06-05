e999e7edee66c2e550685b47ca3de3b1e4749da9
Daniel Santos <danielspovv@gmail.com>
2022-06-05 09:02:09 -0300

    feat: rabbitmq clod connection and postgres dependency

--- src/main/java/com/agile/users/configs/messaging/MessagingConfig.java
+++ src/main/java/com/agile/users/configs/messaging/MessagingConfig.java
@@ -1,15 +1,39 @@
 package com.agile.users.configs.messaging;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessagingConfig {
+  @Value("${messaging.auth.address}")
+  private String address;
+
+  @Value("${messaging.auth.username}")
+  private String username;
+
+  @Value("${messaging.auth.vhost}")
+  private String vhost;
+
+  @Value("${messaging.auth.password}")
+  private String password;
+
+  @Bean
+  public CachingConnectionFactory ccf() {
+    CachingConnectionFactory ccf = new CachingConnectionFactory();
+    ccf.setAddresses(this.address);
+    ccf.setUsername(this.username);
+    ccf.setPassword(this.password);
+    ccf.setVirtualHost(this.vhost);
+    return ccf;
+  }
+
   @Bean
   public MessageConverter messageConverter() {
     return new Jackson2JsonMessageConverter();
